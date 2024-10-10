505e78381ac474a57248b3eaea131dcf41381261
Ashlie29A <ashlie.arora@torontomu.ca>
2024-10-09 22:23:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
+//Javascript document for L5
 alert("Welcome!");
+
+// Button to prompt writing user's name on the homepage
+function buttonClick(){
+    var userName = prompt("What's Your Name?");
+    var nameChange = document.getElementById("Greeting").textContent = "Hello " + userName + "! Welcome to my page.";
+}
+
+// Expanding Image upon user click
+function imageClick(){
+    var image = document.getElementById("self-image"); 
+    if (image.style.width === "302px") { 
+        image.style.width = "604px"; // New width upon click
+        image.style.height = "806px"; // New height upon click
+    } else {
+        image.style.width = "302px"; // Original Width upon re-click
+        image.style.height = "403px"; // Original Height upon re-click
+    }
+}
+
+function mouseOver(navItem) {
+    navItem.style.backgroundColor = "lightblue";
+}
+
+function mouseOut(navItem) {
+    navItem.style.backgroundColor = "";
+}
+
+function ProjectDropdown()
+{
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+    }
+}
+}
+
