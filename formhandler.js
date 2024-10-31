14f45b0c9234596c7f76e6c2cba858b2a0ad5980
Ashlie29A <ashlie.arora@torontomu.ca>
2024-10-30 22:46:43 -0400

    Create formhandler.js

---
+++ formhandler.js
@@ -0,0 +1,9 @@
+    localStorage.setItem('name', name);
+    localStorage.setItem('email', email);
+    localStorage.setItem('gender', gender);
+    localStorage.setItem('favColor', favColor);
+    localStorage.setItem('profession', profession);
+    localStorage.setItem('purpose', purpose);
+    localStorage.setItem('communicationMethods', communicationMethods);
+
+alert("Form submission successful")
