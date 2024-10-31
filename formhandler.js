5ffd2567787d46c615f01304a8735e1b0d0ac234
Ashlie29A <ashlie.arora@torontomu.ca>
2024-10-30 23:51:06 -0400

    Update formhandler.js

--- formhandler.js
+++ formhandler.js
@@ -1,9 +1,27 @@
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
-    localStorage.setItem('gender', gender);
-    localStorage.setItem('favColor', favColor);
-    localStorage.setItem('profession', profession);
-    localStorage.setItem('purpose', purpose);
-    localStorage.setItem('communicationMethods', communicationMethods);
+//Javascript document for handling the form functions   
 
-alert("Form submission successful")
+function submitForm(event) {
+
+ //Saving the name input data in the user's local storage
+    localStorage.setItem("name", document.getElementById("name").value);
+    console.log("Name:", localStorage.getItem("name"));
+ //Saving the email input data in the user's local storage
+    localStorage.setItem("email", document.getElementById("email").value);
+    console.log("Email:", localStorage.getItem("email"));
+ //Saving the gender input data in the user's local storage
+    const gender = document.querySelector('input[name="gender"]:checked')?.value || "Not specified";
+    localStorage.setItem("gender", "Gender");
+    console.log("Gender:", localStorage.getItem("gender"));
+ //Saving the favourite colour input data in the user's local storage
+    localStorage.setItem("favColor", document.getElementById("colorpicker").value);
+    console.log("Favourite Color:", localStorage.getItem("favColor"));
+//Saving the profession input data in the user's local storage
+    localStorage.setItem("profession", document.getElementById("profession").value);
+    console.log("Profession:", localStorage.getItem("profession"));
+    //Saving the purpose input data in the user's local storage
+    localStorage.setItem("purpose", document.querySelector('textarea[name="purpose"]').value);
+    console.log("Purpose:", localStorage.getItem("purpose"));
+}
+
+//Triggers the local storage save when the form is submitted through and event listener
+document.getElementById('myform').addEventListener('submit', submitForm);
