c5e1a4b7db317d936d6a654402145e5351e12d59
Ashlie29A <ashlie.arora@torontomu.ca>
2024-10-30 23:51:10 -0400

    Update lab6main.js

--- lab6main.js
+++ lab6main.js
@@ -1,9 +1,11 @@
-alert("Welcome!");
+//alert("Welcome!");
 
+//Upon hovering on a navigaiton item, the background colour of the navigation item will turn light pink
 function mouseOver(navItem) {
     navItem.style.backgroundColor = "lightpink";
 }
 
+//Once the mouse is removed from hovering over the navigaiotn item, the backgorund colour of the item will revert back
 function mouseOut(navItem) {
     navItem.style.backgroundColor = "";
 }
