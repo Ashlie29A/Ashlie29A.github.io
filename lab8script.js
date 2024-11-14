de8342de472110b15a5ad558f13f9c27775148ea
Ashlie29A <ashlie.arora@torontomu.ca>
2024-11-13 22:39:55 -0500

    Update lab8script.js

--- lab8script.js
+++ lab8script.js
@@ -5,11 +5,11 @@ function increaseSize() {
 
 function changeColours() {
     document.body.style.color='white';
-    document.body.style.backgroundColor='darkgrey';
+    document.body.style.backgroundColor='dimgrey';
 }
 
 function altText() {
-    document.getElementById("image1").innerHTML = "Simba, brown tabby cat and Cajoo, white/brown tabby cat sitting on a wooden staircase facing the camera.";
+    document.getElementById("image1").innerHTML = "Simba, brown tabby cat and Cajoo, white/brown tabby cat sitting on consecutive steps on a wooden staircase facing the camera.";
     document.getElementById("image2").innerHTML = "Cajoo, brown/white tabby cat sitting on a bamboo chair by a window facing the backyard and Simba, brown tabby cat sitting on the windowsill, both looking outside at the backyard.";
     document.getElementById("image3").innerHTML = "Cajoo, brown/white tabby cat laying sprawled out on a bed on his back with all paws stretched out.";
 }
